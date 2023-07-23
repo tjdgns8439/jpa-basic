faaa311801b723c16181a6eb825b891d4e9faa45
seonghun <tjdgns8439@naver.com>
2023-07-23 11:45:16 +0900

    Create Member.java
    멤버 객체 클래스 생성
    - @Entity
    - @Id

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity //JPA를 사용하는 객체라는 것을 인식
+public class Member {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id //pk설정
+    private Long id;
+    private String name;
+}
